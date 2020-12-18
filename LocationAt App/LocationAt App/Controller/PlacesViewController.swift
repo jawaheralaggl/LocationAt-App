9eb461f0c274fe397eefb4a59b469a894f4e5c1f
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-18 20:38:43 +0300

    Set categoriesCollectionView

--- LocationAt App/LocationAt App/Controller/PlacesViewController.swift
+++ LocationAt App/LocationAt App/Controller/PlacesViewController.swift
@@ -11,6 +11,15 @@ class PlacesViewController: UIViewController {
     
     // MARK: - properties
     
+    private let categoriesCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
     private let placesCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -29,9 +38,17 @@ class PlacesViewController: UIViewController {
     // MARK: - layout
     
     func configureUI() {
+        
+        view.addSubview(categoriesCollectionView)
+        categoriesCollectionView.backgroundColor = .white
+        categoriesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        categoriesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        categoriesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        categoriesCollectionView.heightAnchor.constraint(equalToConstant: view.frame.width / 3.5).isActive = true
+        
         view.addSubview(placesCollectionView)
         placesCollectionView.backgroundColor = .white
-        placesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
+        placesCollectionView.topAnchor.constraint(equalTo: categoriesCollectionView.bottomAnchor, constant: -10).isActive = true
         placesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         placesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         placesCollectionView.heightAnchor.constraint(equalToConstant: view.frame.width).isActive = true
