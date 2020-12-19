f0cce15822af3d9ea20087446f9cb6a45b902fbe
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-19 14:22:07 +0300

    Add places data structure

---
+++ LocationAt App/LocationAt App/Model/Places.swift
@@ -0,0 +1,18 @@
+//
+//  Places.swift
+//  LocationAt App
+//
+//  Created by Jawaher Alagel on 12/19/20.
+//
+
+import Foundation
+
+struct Places {
+    var name: String?
+    var id: String?
+    var image_url: String?
+    var rating: Float?
+    var distance: Double?
+    var address: String?
+    var is_closed: Bool?
+}
