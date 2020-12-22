37b1c419cb575b3fde3d04f4058043a8ed189e0b
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-22 14:28:03 +0300

    Create weather struct

---
+++ LocationAt App/LocationAt App/Model/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  LocationAt App
+//
+//  Created by Jawaher Alagel on 12/22/20.
+//
+
+import Foundation
+
+struct Weather {
+    var temp_f: Double?
+    var text: String?
+    var icon: String?
+}
