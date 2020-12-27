56af9a443022eb31d4cec15c99b068f7e910a5ed
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-27 23:20:20 +0300

    Add Lottie library

---
+++ LocationAt App/LocationAt App/Controller/SplashScreen/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  LocationAt App
+//
+//  Created by Jawaher Alagel on 12/27/20.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
